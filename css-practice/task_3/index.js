8a10ea2a776ba1a620ee2b66ed6ff6b59f100d27
dnistrova <natalka.dnistrova@gmail.com>
2018-12-10 10:39:02 +0200

    task 3 fetch

--- css-practice/task_3/index.js
+++ css-practice/task_3/index.js
@@ -1,66 +1,79 @@
-const article1 = {
-  img: 'img/blog1.png',
-  previewImg: 'img/footer-image-1.png',
-  day: '15',
-  month: 'Jan',
-  title: 'Lorem ipsum dolor sit amet',
-  watched: '542',
-  comments: '17',
-  date: 'Jan 9, 2016',
-};
-const articles = document.querySelector('.three-images.blog-articles');
-const blogs = document.querySelector('.footer-blogs');
+const articlesSection = document.querySelector('.blog-articles');
+const blogsSection = document.querySelector('.footer-blogs');
 
-function renderLatestArticle(image, day, month, heading, viewsCounter, commentsCounter) {
-  const element = document.createElement('DIV');
-  element.classList.add('template-article');
+function RenderArticleItem(model) {
+  const {previewImg, title, description, watched, comments, published} = model;
+  const day = new Date(published).getDate();
+  const month = formatMonth(published);
+  const element = document.createElement('article');
 
   const article = `
-    <article class="template-article">
-      <img src="${image}" alt="">
+      <img src="${previewImg}" alt="">
       <div class="calendar">
         <p><span>${day}</span> ${month}</p>
       </div>
       <div class="latest-blog-paragraph">
-        <h2>${heading}</h2>
-        <p>Consectetur adipiscing elit, sed do eiusmod 
-        tempor incididunt ut labore et dolore magna aliqua.</p>
+        <h2>${title}</h2>
+        <p>${description}</p>
       </div>
       <div class="counter">
-        <p><span><i class="far fa-eye"></i></span> ${viewsCounter}</p>
-        <p><i class="fas fa-comment-dots"></i> ${commentsCounter}</p>
+        <p><span><i class="far fa-eye"></i></span> ${watched}</p>
+        <p><i class="fas fa-comment-dots"></i> ${comments}</p>
       </div>
-    </article>
   `;
 
   element.innerHTML = article;
-  articles.append(element);
+
+  return (element);
 }
 
-renderLatestArticle(article1.img,
-    article1.day, article1.month,
-    article1.title, article1.watched,
-    article1.comments);
+function renderer(res) {
+  res.blogs.forEach(((model) => {
+    blogsSection.appendChild(new RenderBlogItem(model));
+  }));
+  const blogs = res.blogs.filter((blog) => {
+    return res.latest.includes(blog.id);
+  });
+  blogs.forEach(((model)=> {
+    articlesSection.appendChild(new RenderArticleItem(model));
+  }));
+}
 
-function renderBlogArticle(image, heading, date) {
-  const element = document.createElement('DIV');
+function formatMonth(published) {
+  const date = new Date(published);
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+  return months[date.getMonth()];
+}
+
+function RenderBlogItem(model) {
+  const {previewImg, title, published} = model;
+  const date = showFormattedDate(new Date(published));
+  const element = document.createElement('div');
   element.classList.add('footer-blog');
 
   const blog = `
     <div class="footer-blog-image">
-      <img src="${image}" alt="">
+      <img src="${previewImg}" alt="">
     </div>
     <div class="footer-blog-text">
-      <p>${heading}</p>
+      <p>${title}</p>
       <h5 class="footer-blog-date">${date}</h5>
-    </div>
     </div>
   `;
 
   element.innerHTML = blog;
-  blogs.append(element);
+  return (element);
 }
 
-renderBlogArticle(article1.previewImg,
-    article1.title,
-    article1.date);
+function showFormattedDate(published) {
+  const date = new Date(published);
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const MMM = months[date.getMonth()];
+
+  return MMM + ' ' + date.getDate() + ', ' + date.getFullYear();
+}
+
+fetch('http://localhost:3000/api/blogs')
+  .then(response => response.json())
+  .then(renderer);
